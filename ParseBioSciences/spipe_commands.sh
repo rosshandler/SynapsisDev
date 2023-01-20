4c6389e3356c48c62b063636e2dda6da12e545a6
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2023-01-20 21:28:32 +0000

    Update spipe_commands.sh

--- ParseBioSciences/spipe_commands.sh
+++ ParseBioSciences/spipe_commands.sh
@@ -10,14 +10,14 @@ split-pipe \
 --mode mkref \
 --genome_name hg38 mm10 \
 --fasta $PBS/newvolume/genomes/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz $PBS/newvolume/genomes/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz \
---genes $PBS/newvolume/genomes/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz $PBS/newvolume/genomes/Mus_musculus.GRCm38.93.gtf.gz \
+--genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.93.gtf.gz $PBS/newvolume/genomes/Mus_musculus.GRCm38.93.gtf.gz \
 --output_dir $PBS/newvolume/genomes/hg38_mm10
 
 split-pipe \
 --mode mkref \
 --genome_name hg38 \
 --fasta $PBS/newvolume/genomes/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz \
---genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.108.gtf.gz \
+--genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.93.gtf.gz \
 --output_dir $PBS/newvolume/genomes/hg38
 
 split-pipe \
