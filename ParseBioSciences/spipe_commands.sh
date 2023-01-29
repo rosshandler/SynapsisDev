e94cb58927056b179360022ef8c8a3c1dadcd7e4
rosshandler <ivanir@mrc-lmb.cam.ac.uk>
2023-01-29 22:34:06 +0000

    Update spipe_commands.sh

--- ParseBioSciences/spipe_commands.sh
+++ ParseBioSciences/spipe_commands.sh
@@ -5,27 +5,41 @@ PBS='/data2/ivanir/Feline2023/ParseBS'
 
 PATH="/data2/ivanir/Feline2023/ParseBS/ParseBiosciences-Pipeline.1.0.3p:$PATH"
 
-# Genome indexing
-split-pipe \
---mode mkref \
---genome_name hg38 mm10 \
---fasta $PBS/newvolume/genomes/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz $PBS/newvolume/genomes/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz \
---genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.93.gtf.gz $PBS/newvolume/genomes/Mus_musculus.GRCm38.93.gtf.gz \
---output_dir $PBS/newvolume/genomes/hg38_mm10
+cd $PBS/newvolume/genomes/
+cat Homo_sapiens.GRCh38.108.gtf EmGFP.gtf > Homo_sapiens.GRCh38.108.EmGFP.gtf
+cat Homo_sapiens.GRCh38.dna.primary_assembly.fa EmGFP.fa > Homo_sapiens.GRCh38.dna.primary_assembly.EmGFP.fa
+
+cat Mus_musculus.GRCm39.108.gtf EmGFP.gtf > Mus_musculus.GRCm38.108.EmGFP.gtf
+cat Mus_musculus.GRCm39.dna.primary_assembly.fa EmGFP.fa > Mus_musculus.GRCm38.dna.primary_assembly.EmGFP.fa
 
 split-pipe \
 --mode mkref \
 --genome_name hg38 \
---fasta $PBS/newvolume/genomes/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz \
---genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.93.gtf.gz \
+--fasta $PBS/newvolume/genomes/Homo_sapiens.GRCh38.dna.primary_assembly.EmGFP.fa \
+--genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.108.EmGFP.gtf \
 --output_dir $PBS/newvolume/genomes/hg38
 
 split-pipe \
 --mode mkref \
 --genome_name mm10 \
---fasta $PBS/newvolume/genomes/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz \
---genes $PBS/newvolume/genomes/Mus_musculus.GRCm38.93.gtf.gz \
+--fasta $PBS/newvolume/genomes/Mus_musculus.GRCm38.dna.primary_assembly.EmGFP.fa \
+--genes $PBS/newvolume/genomes/Mus_musculus.GRCm38.108.EmGFP.gtf \
 --output_dir $PBS/newvolume/genomes/mm10
+
+cat Homo_sapiens.GRCh38.108.gtf hEmGFP.gtf > Homo_sapiens.GRCh38.108.hEmGFP.gtf
+cat Homo_sapiens.GRCh38.dna.primary_assembly.fa hEmGFP.fa > Homo_sapiens.GRCh38.dna.primary_assembly.hEmGFP.fa
+
+cat Mus_musculus.GRCm39.108.gtf mEmGFP.gtf > Mus_musculus.GRCm38.108.mEmGFP.gtf
+cat Mus_musculus.GRCm39.dna.primary_assembly.fa mEmGFP.fa > Mus_musculus.GRCm38.dna.primary_assembly.mEmGFP.fa
+
+# Genome indexing
+split-pipe \
+--mode mkref \
+--genome_name hg38 mm10 \
+--fasta $PBS/newvolume/genomes/Homo_sapiens.GRCh38.dna.primary_assembly.EmGFP.fa $PBS/newvolume/genomes/Mus_musculus.GRCm38.dna.primary_assembly.EmGFP.fa \
+--genes $PBS/newvolume/genomes/Homo_sapiens.GRCh38.108.EmGFP.gtf $PBS/newvolume/genomes/Mus_musculus.GRCm38.108.EmGFP.gtf \
+--output_dir $PBS/newvolume/genomes/hg38_mm10
+
 
 cd $PBS/newvolume/expdata/
 cat SLX-22602.DNAA007.HGMLNDMXY.s_1.r_1.fq.gz SLX-22602.DNAA007.HGMLNDMXY.s_2.r_1.fq.gz > SLX-22602.r_1.fq.gz
